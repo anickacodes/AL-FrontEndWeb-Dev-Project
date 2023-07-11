f8269ff3e06d7df57f4e5a804a7d064c8a90cd15
anickacodes <anickalewis@pursuit.org>
2023-07-11 00:54:23 -0400

    practice fetch, failed

---
+++ app.js
@@ -0,0 +1,12 @@
+// const ttry = fetch('https://amazon-price1.p.rapidapi.com/search?keywords=%3CREQUIRED%3E&marketplace=ES')
+// console.log(ttry)
+// try {
+//   const response = await fetch(
+//     "https://amazon-price1.p.rapidapi.com/search?keywords=%3CREQUIRED%3E&marketplace=ES",
+//     options
+//   );
+//   const result = await response.text();
+//   console.log(result);
+// } catch (error) {
+//   console.error(error);
+// }
