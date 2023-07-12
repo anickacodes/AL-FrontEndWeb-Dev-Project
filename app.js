402a0c4198996696bdc933b454eec9a85e7d0c71
anickacodes <anickalewis@pursuit.org>
2023-07-12 12:21:56 -0400

    redirected as per dev team's aka Gary's suggestion, did boilerplate in html, attempting a new api 😔, started css styling

--- app.js
+++ app.js
@@ -1,17 +1,8 @@
-// const ttry = fetch('https://amazon-price1.p.rapidapi.com/search?keywords=%3CREQUIRED%3E&marketplace=ES')
-// .then((response)=> response.json())
-// .then((JSONresponse)=> console.log(JSONresponse))
-// console.log(ttry)
 
 
-// const url = '';
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'b89fb8faa9msh40a677f6078ef48p1f0f40jsnb7ca42e40fb0',
-		'X-RapidAPI-Host': 'ali-express1.p.rapidapi.com'
-	}
-};
-fetch('https://ali-express1.p.rapidapi.com/product/4000479928418?language=en', options)
-.then((response)=> response.json())
-.then((joy)=> console.log(joy))
+console.log("yo");
+
+
+fetch("https://pokeapi.co/api/v2/type/18/")
+  .then((response) => response.json())
+  .then((joy) => console.log(joy.pokemon));
